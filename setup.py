7ef19cca04ece08fd7d6aa6687c3f43e9d07d0ff
Rolando Sotelo <rola@hey.com>
2022-02-04 23:47:25 -0600

    ⚙️  CONFIGURATION: Update setup.py for package to work

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import find_packages, setup
 
-
-with open('README.md','r') as f:
+with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(
@@ -13,5 +12,10 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/rolasotelo/pgbackup',
-    packages=find_packages('src')
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    entry_points={
+        'console_scripts': ['pgbackup=pgbackup.cli:main']
+    }
 )
